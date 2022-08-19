7358fd9c25112b4166a94aa34f4f10b801ad62ef
Bharatdeep Hazarika <67242026+Bajrangi18@users.noreply.github.com>
2022-08-19 21:48:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy
+# import numpy
 import cv2 
 def hello(name = 123):  
 
-    return numpy.__version__ + " "+ str(name) + " " + cv2.__version__ 
+    return str(name) + cv2.__version__ 
